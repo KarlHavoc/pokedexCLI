a78df021eb8fe5300057a35999964e21085b1760
carlhavoc <mkowalsky87@gmail.com>
2025-01-29 15:01:35 -0700

    just added user input functionality

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KarlHavoc/pokedex
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userText := scanner.Text()
+		cleanText := cleanInput(userText)
+		fmt.Println(cleanText[0])
+	}
+
+}
+
+func cleanInput(text string) []string {
+	splitStrings := strings.Split(text, " ")
+	lowered := []string{}
+	for _, word := range splitStrings {
+		if word == "" {
+			continue
+		}
+		l := strings.ToLower(word)
+		lowered = append(lowered, l)
+	}
+	return lowered
+
+}
